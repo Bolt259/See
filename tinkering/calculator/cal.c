11dd33c98ba36fdc7262d23949528db260e07633
Bolt259 <97069761+Bolt259@users.noreply.github.com>
2023-11-07 08:55:40 -0800

    added error handling sort of

--- tinkering/calculator/cal.c
+++ tinkering/calculator/cal.c
@@ -4,29 +4,37 @@ int main() {
     float a, b, result;
     char op;
 
-    printf("Calculator\nType in an expression: \n");
+    while (1) {
+        printf("Calculator\nType in an expression: \n");
 
-    // Assigns variables to terms in the expression
-    scanf("%f %c %f", &a, &op, &b);
+        if (scanf("%c", &op) == 1){
+            if(op == 'q' || op == 'Q'){
+                break;
+            }
+        } else {
+            // Assigns variables to terms in the expression
+            scanf("%f %c %f", &a, &op, &b);
+        }
 
-    // Depending on the operator returns the result
-    switch(op) {
-        case '+':
-            result = a + b;
-            break;
-        case '-':
-            result = a - b;
-            break;
-        case '*':
-            result = a * b;
-            break;
-        case '/':
-            result = a / b;
-            break;
-        default:
-            printf("Unknown operation...");
-            break;
+        // Depending on the operator returns the result
+        switch(op) {
+            case '+':
+                result = a + b;
+                break;
+            case '-':
+                result = a - b;
+                break;
+            case '*':
+                result = a * b;
+                break;
+            case '/':
+                result = a / b;
+                break;
+            default:
+                printf("Unknown operation...");
+                break;
+        }
+        printf("\nThe result is: %.4f\n", result);
     }
-    printf("\nThe result is: %.4f", result);
     return 0;
 }
